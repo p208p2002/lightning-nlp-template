185845bed0bd4b35450a115dd9229d0771379f20
Philip Huang <p208p2002@gmail.com>
2023-02-01 14:13:35 +0800

    chore: update the args of the callbacks

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from pytorch_lightning.callbacks import ModelCheckpoint,EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from argparse import ArgumentParser
 from pytorch_lightning import Trainer
-from core import BertNSP,STCDataModule
+from core import BertNSP, STCDataModule
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser = Trainer.add_argparse_args(parser)    
+    parser = Trainer.add_argparse_args(parser)
     parser.add_argument('--learning_rate', '-lr', type=float, default=5e-6)
     parser.add_argument('--batch_size', '-bs', type=int, default=8)
     args = parser.parse_args()
@@ -15,21 +15,27 @@ if __name__ == "__main__":
     best_checkpoint_callback = ModelCheckpoint(
         monitor='val_loss',
         filename='best',
-        save_on_train_epoch_end=True,
+        save_on_train_epoch_end=False,
         save_top_k=1,
     )
 
     val_checkpoint_callback = ModelCheckpoint(
         monitor='val_loss',
-        filename='val-epoch={epoch:02d}-setp={step}-val_loss={val_loss:.2f}',
+        filename='val-epoch={epoch:02d}-setp={step}-val_loss={val_loss:.5f}',
         save_last=True,
-        save_on_train_epoch_end=True,
+        save_on_train_epoch_end=False,
         save_top_k=3
     )
 
-    early_stopping = EarlyStopping('val_loss',mode='min',patience=4)
+    early_stopping = EarlyStopping(
+        'val_loss',
+        mode='min',
+        patience=5,
+        check_on_train_epoch_end=False
+    )
 
-    trainer = Trainer.from_argparse_args(args, callbacks=[val_checkpoint_callback,early_stopping])
+    trainer = Trainer.from_argparse_args(args, callbacks=[
+                                         best_checkpoint_callback, val_checkpoint_callback, early_stopping])
     datamodule = STCDataModule(batch_size=args.batch_size)
 
     trainer.fit(model, datamodule=datamodule)
